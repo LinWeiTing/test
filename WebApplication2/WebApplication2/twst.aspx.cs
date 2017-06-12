ae3db7241ff7fd4769d81448e9314f9b7dc59d97
LinWeiTing <admins.wei@gmail.com>
2017-06-13 03:03:11 +0800

    l.w.t. alive2
    alive pool

--- WebApplication2/WebApplication2/twst.aspx.cs
+++ WebApplication2/WebApplication2/twst.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication2.Service;
 
 namespace WebApplication2
 {
     public partial class twst : System.Web.UI.Page
     {
+        private LineChatService _lineChatService;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            _lineChatService = new LineChatService();
+            if (_lineChatService.CheckProvider("0952259389", "sp036239", true))
+            {
+            }
+            else 
+            { 
+            }
         }
     }
 }
